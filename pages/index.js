3f641d4ed9e3e06dc4e2ada60b15d1681bf48fb9
kstrive100 <126906802+kstrive100@users.noreply.github.com>
2023-04-26 23:53:09 +0400

    11

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Grad from "@/utils/gradient"
 export default function Home() {
   return (
     <div className={montserrat.className}>
-      {/* <Image className="starImage" src='/star.svg' alt="logo" width="1175" height="1170" /> */}
+      <Image className="starImage" src='/star.svg' alt="logo" width="1175" height="1170" />
       <NavBar />
       <FirstBlock />
       <Grad />
