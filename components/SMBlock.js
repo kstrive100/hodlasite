3e435913d3121315a00186a513d29850e26bf8a4
kstrive100 <126906802+kstrive100@users.noreply.github.com>
2023-05-02 18:24:46 +0400

    ffff

--- components/SMBlock.js
+++ components/SMBlock.js
@@ -1,13 +1,17 @@
 import { formula } from '@/utils/fonts'
+import { motion } from 'framer-motion';
 
 import styles from '@/styles/smBlock.module.css'
 
 export default function SMBlock() {
     return (
         <div className={`${styles.main} ${formula.className}`}>
-            <h1>
-                join our community
-            </h1>
+            <div className={styles.textSlide}>
+                <h1>join our community</h1>
+            </div>
+            <div className={styles.textSlide}>
+                <h1>join our community</h1>
+            </div>
         </div>  
     )
 }
